ef46c07dd20652ea39ba1b308830e15b6296d66f
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2026-03-15 13:17:02 +0530

    Update and rename Bankers to Bankers.c

--- Exp_09/Bankers.c
+++ Exp_09/Bankers.c
@@ -1,71 +1,102 @@
+
 #include<stdio.h>
+
 int main(){
 	int np,nr;
+
 	printf("Enter Number of Processes : ");
 	scanf("%d",&np);
+
 	printf("Enter Number of Resources: ");
 	scanf("%d",&nr);
+
 	int allocation[10][10];
 	int maximum[10][10];
 	int need[10][10];
 	int available[10];
-	
+
 	printf("Enter Allocation Matrix:\n");
 	for(int i=0;i<np;i++){
 		for(int j=0;j<nr;j++){
 			scanf("%d",&allocation[i][j]);
 		}
 	}
+
 	printf("Enter Maximum Matrix:\n");
 	for(int i=0;i<np;i++){
 		for(int j=0;j<nr;j++){
 			scanf("%d",&maximum[i][j]);
 		}
 	}
+
 	printf("Enter Available Resources:\n");
 	for(int i=0;i<nr;i++){
 		scanf("%d",&available[i]);
 	}
+
+	// Calculate Need Matrix
 	for(int i=0;i<np;i++){
 		for(int j=0;j<nr;j++){
-			need[i][j]=maximum[i][j]-allocation[i][j];
+			need[i][j] = maximum[i][j] - allocation[i][j];
 		}
 	}
-	int finished[np],safeSequence[np];
+
+	// Display Need Matrix
+	printf("\nNeed Matrix:\n");
 	for(int i=0;i<np;i++){
-		finished[i]=0;
+		for(int j=0;j<nr;j++){
+			printf("%d ",need[i][j]);
+		}
+		printf("\n");
 	}
-	int count =0;
-	while(count<np){
-		int processFound=0;
+
+	int finished[10], safeSequence[10];
+
+	for(int i=0;i<np;i++){
+		finished[i] = 0;
+	}
+
+	int count = 0;
+
+	while(count < np){
+		int processFound = 0;
+
 		for(int i=0;i<np;i++){
-			if(finished[i]==0){
+			if(finished[i] == 0){
+
 				int canRun = 1;
+
 				for(int j=0;j<nr;j++){
-					if(need[i][j]>available[j]){
-						canRun=0;
+					if(need[i][j] > available[j]){
+						canRun = 0;
 						break;
 					}
 				}
+
 				if(canRun == 1){
 					for(int j=0;j<nr;j++){
-						available[j]+=allocation[i][j];
+						available[j] += allocation[i][j];
 					}
-					safeSequence[count]=i;
+
+					safeSequence[count] = i;
 					count++;
-					finished[i]=1;
-					processFound=1;
+					finished[i] = 1;
+					processFound = 1;
 				}
 			}
 		}
-		if(processFound==0){
-			printf("System is NOT in safe state\n");
+
+		if(processFound == 0){
+			printf("\nSystem is NOT in safe state\n");
 			return 0;
 		}
 	}
-	printf("System is in safe state\nSafe sequence: ");
+
+	printf("\nSystem is in safe state\nSafe sequence: ");
 	for(int i=0;i<np;i++){
-		printf("P%d \n",safeSequence[i]);
+		printf("P%d ",safeSequence[i]);
 	}
+
 	return 0;
 }
+
